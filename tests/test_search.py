ffd46fd37c7f166a79fb28762df61328b796c1b6
bdclauser <pzljug@hotmail.com>
2023-03-03 02:20:16 -0500

    update framework and initial test

---
+++ tests/test_search.py
@@ -0,0 +1,23 @@
+"""
+Using Duck Duck Go
+"""
+
+
+def test_basic_duckduckgo_search():
+
+    # Given the DuckDuckGO home page is displayed
+    # TODO
+
+    # When the user searches for "panda"
+    # TODO
+
+    # Then the search result title contains "panda"
+    # TODO
+
+    # And the search result query is "panda"
+    # TODO
+
+    # And the search result links to "panda"
+    # TODO
+
+    raise Exception("Incomplete Test")
